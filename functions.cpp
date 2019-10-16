b07fabd713c28f525cf247cba1ce7e7ce51d2445
hectortav <tavhector@gmail.com>
2019-10-16 13:22:51 +0300

    not much... :(

--- functions.cpp
+++ functions.cpp
@@ -1,1 +1,22 @@
 #include "functions.h"
+#include <math.h>
+
+void create_hist(relation *relR)
+{
+    int hist[pow(2, 8)][2] //static array?
+    int64_t key;
+    int i;
+    for(i = 0; i < pow(2, 8); i++)
+    {
+        hist[i][0]= i;
+        hist[i][1]= 0;
+    }
+
+    for (i = 0; i < relation.num_tuples; i++)
+    {
+        key = (relR.tuples[i].key >> 56) & 0xFF;
+        hist[key][1]++;
+    }
+    
+
+}
--- functions.h
+++ functions.h
@@ -5,18 +5,21 @@
 #include <cstring>
 #include "list.h"
 #include <ctime>
+
 class tuple
 {
 public:
     int64_t key;
     int64_t payload;
 };
+
 class relation
 {
 public:
     tuple* tuples;
     uint64_t num_tuples;
 };
+
 class result
 {
 
