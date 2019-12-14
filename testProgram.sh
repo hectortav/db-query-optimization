1f66650b41c6b847644959f69895ecfb25970c19
AntonisKl <antonisklironomos@gmail.com>
2019-12-14 18:34:54 +0200

    testProgram.sh: Removed redundant comments

--- testProgram.sh
+++ testProgram.sh
@@ -5,16 +5,11 @@ startProgramAndCalculateTime() {
     
     while read path action file
     do
-        # echo "The file '$file' appeared/changed in directory '$path' via '$action'"
         if [[ $file == "read_arrays_end" ]]; then
             
             startTimestamp=$(date +%s%N)
-            # cat $workFile > P
             exec 3>&-
         elif [[ $file == $outputFile ]] && [[ $action == "MODIFY" ]]; then
-            # echo "$(wc -l < $outputFile)"
-            # echo "results: $resultsLinesNum"
-            # echo "The file '$file' appeared in directory '$path' via '$action'"
             if [[ $resultsLinesNum == $(wc -l < $outputFile) ]]; then
                 endTimestamp=$(date +%s%N)
                 elapsedTimeNanoseconds=$((endTimestamp-startTimestamp))
@@ -75,39 +70,11 @@ do
     case $inputChar in
         s)
             echo "Running program with \"small\" workload..."
-            #cat $workloadsPath/small/small.init $workloadsPath/small/small.work | ./final > out.txt
             initFile="$workloadsPath/small/small.init"
             resultsFile="$workloadsPath/small/small.result"
             workFile="$workloadsPath/small/small.work"
             outputFile="small_ouput.txt"
             resultsLinesNum=$(wc -l < $resultsFile)
-            # mkfifo mfifo
-            # cat > mfifo &
-            # mpid=$!
-            # ./final < mfifo
-            # input=`cat ./input/inputFileNames.txt`
-            # echo "ok1"
-            # echo "$input" > mfifo
-            # #cat ./input/inputFileNames.txt > mfifo 
-            # echo "ok2"
-            # #start timer
-            # input=`cat ./input/inputQueries.txt` 
-            # echo "$input" > mfifo 
-            # echo EOF > mfifo
-            # #cat ./input/inputQueries.txt > mfifo
-            # kill $mypid
-            # rm mfifo
-
-            # mkfifo P
-            # ./final < P > $outputFile &
-            # exec 3>P # open file descriptor 3 writing to the pipe
-
-            # < P tail -n +1 -f | program
-            # cat $initFile > P
-            # cat more_stuff.txt > P
-            # cat ../workloads/small/small.init - | ./final
-                    # cat $workFile > P
-                    # exec 3>&-
             startProgramAndCalculateTime
             ;;
         m)
@@ -118,10 +85,7 @@ do
             workFile="$workloadsPath/medium/medium.work"
             outputFile="medium_ouput.txt"
             resultsLinesNum=$(wc -l < $resultsFile)
-
             startProgramAndCalculateTime
-
-            # cat $workloadsPath/medium/medium.init $workloadsPath/medium/medium.work | ./final > out.txt
             ;;
         *)
             echo -e "\nInvalid character, please try again\n"
