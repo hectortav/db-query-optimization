e2c5749554cd11787b249ada77ec30eae1b1b864
AntonisKl <antonisklironomos@gmail.com>
2020-01-21 22:06:21 +0200

    Added comments to besttreemap.h

--- besttreemap.h
+++ besttreemap.h
@@ -73,20 +73,25 @@ public:
 
 class InputArray;
 
+// performs a Join Enumeration algorithm to find the best execution plan for the currently handled query
 uint64_t** BestPredicateOrder(uint64_t** currentpreds,int cntr,int relationsum,int*relationids,const InputArray** inputarr,ColumnStats** Stats );
+// returns the correctly reordered predicates according to Join Enumeration (calls FilterStats() and BestPredicateOrder())
 uint64_t** OptimizePredicates(uint64_t**,int&,int,int*,const InputArray**);
+// calculates the ColumnStats of the arrays that take part in filters
 void FilterStats(uint64_t** filterpreds,int cntr,int relationsum,int*relationids,const InputArray** inputarr,ColumnStats** Stats);
+// returns the number of all the current combinations of size combinationSize
 int getCombinationsNum(int size, int combinationSize);
+// constructs all the possible combinations (PredicateArray objects) of size r and stores them in resultArray
 void getCombinations(PredicateArray* elements, int n, int r, int index, PredicateArray* data,
-                     PredicateArray* resultArray, int i, int& nextIndex);
-                     
+                     PredicateArray* resultArray, int i, int& nextIndex);  
+// updates the ColumnStats for the array with id predicateArrayId for all the columns except the one with index joinFieldId
 void updateColumnStats(const InputArray* pureInputArray, uint64_t joinFieldId, int predicateArrayId,
                          Value* valueP, Value* newValueP, ColumnStats** filterColumnStatsArray,
-                         uint64_t fieldDistinctValuesNumAfterFilter, uint64_t fieldValuesNumBeforeFilter);
-                         
+                         uint64_t fieldDistinctValuesNumAfterFilter, uint64_t fieldValuesNumBeforeFilter);           
+// calculates and updates the ColumnStats of newValueP (the new join chain which includes newPredicateP) by using the ColumnStats of valueP (the old join chain)
 void updateValueStats(Value* valueP, Value* newValueP, Predicate* newPredicateP, int* relationIds,
                          ColumnStats** filterColumnStatsArray, const InputArray** inputArrays);
-
+// constructs and returns a new Value (new join chain) which contains newPredicateArrayP
 Value* createJoinTree(Value* valueP, PredicateArray* newPredicateArrayP, int* relationIds, int relationsnum,
                          ColumnStats** filterColumnStatsArray, const InputArray** inputArrays);
 
